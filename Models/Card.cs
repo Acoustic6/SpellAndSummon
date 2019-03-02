9f17d951e247de2796d43a938c42e33b5fcef886
Acoustic6 <6dorog@mail.ru>
2019-03-02 17:21:16 +0300

    changing cards table name to correct one

--- Models/Card.cs
+++ Models/Card.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SpellAndSummon.Models
 {
-    [Table("Tables")]
+    [Table("Cards")]
     public class Card
     {
         public int Id { get; set; }
