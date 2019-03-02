6bcc5d7258503c0a47377dda2dfa3ecddb3db02e
Acoustic6 <6dorog@mail.ru>
2019-03-02 17:19:52 +0300

    changing cards and decks tables

--- Models/Card.cs
+++ Models/Card.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SpellAndSummon.Models
 {
+    [Table("Tables")]
     public class Card
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public Deck Deck {get; set;}
         public int DeckId {get; set;}
--- Models/Deck.cs
+++ Models/Deck.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SpellAndSummon.Models
 {
     public class Deck
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public ICollection<Card> Cards {get; set;}
         public Deck() => this.Cards = new Collection<Card>();
