5c9d6c5d5ee2c07a03fcd05361798c8475493e7b
Acoustic6 <6dorog@mail.ru>
2019-04-14 00:07:19 +0300

    special abilities' props added

---
+++ Migrations/20190413210601_SpecialAbilitiesPropsAdded.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpellAndSummon.Migrations
+{
+    public partial class SpecialAbilitiesPropsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Params",
+                table: "SpecialAbilities",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "SpecialAbilities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Params",
+                table: "SpecialAbilities");
+
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "SpecialAbilities");
+        }
+    }
+}
--- Models/SpecialAbility.cs
+++ Models/SpecialAbility.cs
@@ -10,6 +10,8 @@ namespace SpellAndSummon.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Text { get; set; }
+        public string Params { get; set; }
         public ICollection<SpecialAbilityCard> SpecialAbilityCards { get; set; }
         public SpecialAbility()
         {
